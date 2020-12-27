5ea1a5e67f7e6e53b386ddbb52798525491fa475
matveybaykalov <70635348+matveybaykalov@users.noreply.github.com>
2020-12-27 19:01:24 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <filesystem>
 #include <string>
-#include <vector>
+#include <sstream>
+#include <exception>
 
 namespace fs = std::filesystem;
 using std::cout;
@@ -9,18 +10,13 @@ using std::cin;
 using std::endl;
 using std::string;
 
-void PrintPath(std::string path){
-    //path.erase(0,10); //удаление странной приписки в начале (скорее всего приписка компилятора)
-    cout << "Path is " << path << endl;
-}
-
 void WriteDirectoryFiles(const fs::path& MyPath){
     for (auto& p : fs::directory_iterator(MyPath)){
         cout << p.path().filename() << endl;
     }
 }
 
-bool IsDirectoryExist (string& MyPath, string& directory){
+bool IsDirectoryExist (const fs::path& MyPath, std::filesystem::path directory){
     for (auto& p : fs::directory_iterator(MyPath)){
         if(p.path().filename() == directory){
             return true;
@@ -29,28 +25,21 @@ bool IsDirectoryExist (string& MyPath, string& directory){
     return false;
 }
 
-void CreateDirectory(const string& name, fs::path& FullPath){
-    std::vector <string> PathInVec;
-    string StringPath = FullPath;
-    StringPath += '/' + name;
-    string MyPath;
-    while (StringPath.find('/') != -1){                                          //перевод всего переданного адреса в вектор,
-        PathInVec.push_back(StringPath.substr(0, StringPath.find('/'))); //делавется для того, чтобы если будет укзана путь к
-        StringPath.erase(0, StringPath.find('/')+1);                     //через несуществующую директорию, то она будет создана
-    }
-    PathInVec.push_back(StringPath);
-    for (int i = 0; i < PathInVec.size()-2; ++i){
-        MyPath += PathInVec[i] + '/';
-        if(!IsDirectoryExist(MyPath, PathInVec[i+1])){
-            fs::create_directory(MyPath + PathInVec[i+1]);
+void copy (fs::path& from, fs::path& to){ //создал функцию copy, потому что библиотечная функция не копирует вложенные директории
+    fs::path CopyOfFrom = to;
+    CopyOfFrom.remove_filename();
+    if (!IsDirectoryExist(CopyOfFrom, to.filename())){
+        fs::copy(from, to);//это библиотечная функция
+        for (auto &p : fs::directory_iterator(from)) {
+            if (fs::is_directory(p)) {
+                fs::path temp1 = from / p.path().filename();  //временные переменные использую, потому что компилятор
+                fs::path temp2 = to / p.path().filename();    //выдаёт ошибку при записи  copy(from/p.path().filename(), to/p.path().filename());
+                //возможно это связано с тем, что моя функция и библиотечная имеют одинаковые имена
+                copy(temp1, temp2); //это моя функция
+            }
         }
-    }
-    MyPath += PathInVec[PathInVec.size()-2] + '/';
-    if (IsDirectoryExist(MyPath, PathInVec[PathInVec.size()-1])){
-        cout << "Directory " << PathInVec[PathInVec.size()-1] << " is exist" << endl;
     } else {
-        fs::create_directories(MyPath+PathInVec[PathInVec.size()-1]);
-        cout << "Directory "<< PathInVec[PathInVec.size()-1] <<" was created successfully" << endl;
+        throw std::invalid_argument(to.string() + " is exist");
     }
 }
 
@@ -61,19 +50,12 @@ int main() {
         if (command == "show_files"){
             WriteDirectoryFiles(FullPath);
         } else if(command == "print_path"){
-            PrintPath(FullPath);
-        } else if(command == "back"){
-            FullPath.remove_filename();
-            string temp;
-            temp = FullPath;
-            temp.erase(temp.size()-1);
-            FullPath = temp;
-            WriteDirectoryFiles(FullPath);
-        } else if(command == "goto"){
-            string floor;
-            cin >> floor;
-            fs::path TempPath = FullPath;
-            TempPath += '/' + floor;
+            cout << FullPath << endl;
+        } else if(command == "exit"){
+            break;
+        } else if(command == "set_path"){
+            fs::path TempPath;
+            cin >> TempPath;
             try {
                 WriteDirectoryFiles(TempPath);
             } catch (std::exception& e) {
@@ -81,74 +63,92 @@ int main() {
                 continue;
             }
             FullPath = TempPath;
+        } else if(command == "goto"){
+            string floor;
+            std::getline(cin, floor);
+            floor.erase(0,1);
+            fs::path TempPath = FullPath;
+            TempPath += '\\' + floor; //временный путь нужен для проверки корректности
+            try {
+                WriteDirectoryFiles(TempPath);
+            } catch (std::exception& e) {
+                cout << e.what() << endl;
+                continue;
+            }
+            FullPath = TempPath;
+        } else if(command == "back"){
+            FullPath.remove_filename();
+            string temp;
+            std::stringstream ss; //создаю поток, чтобы перевести path в string. После чего удалить два поледних
+            ss << FullPath;       //слеша, который остались после remove_filename()
+            ss >> temp;
+            temp.erase(temp.size()-3);
+            temp.erase(0, 1);
+            while (temp.find("\\\\") != -1){                         //удаляю экранированные слеши из строчки, потому что
+                temp.replace(temp.find("\\\\"), 2, "\\"); //при присваивании FullPath = temp слеши дополнительно экранируются
+            }
+            FullPath = temp;
+            WriteDirectoryFiles(FullPath);
         } else if(command == "create_directory"){
             string floor;
-            cin >> floor;
-            CreateDirectory(floor, FullPath);
+            std::getline(cin,floor);
+            floor.erase(0, 1);
+            if (!(IsDirectoryExist(FullPath, floor))){
+                fs::path TempPath = FullPath;
+                TempPath += '\\' + floor;
+                try {
+                    fs::create_directory(TempPath);
+                    cout << "successfully" << endl;
+                } catch (std::exception &e) {
+                    cout << e.what() << endl;
+                    continue;
+                }
+                FullPath = TempPath;
+            } else {
+                cout << floor << " is exist" << endl;
+            }
         } else if(command == "size"){
             string FileName;
             fs::path TempPath;
-            cin >> FileName;
+            std::getline(cin, FileName);
+            FileName.erase(0, 1);
             TempPath = FullPath;
-            TempPath += '/' + FileName;
+            TempPath += '\\' + FileName;
             try {
                 std::cout << "File size = " << fs::file_size(TempPath) << endl;
             } catch(fs::filesystem_error& e) {
                 std::cout << e.what() << endl;
             }
-        } else if(command == "exit"){
-            break;
-        } else if(command == "set_path"){
-            fs::path MyPath;
-            cin >> MyPath;
-            fs::path TempPath;
-            TempPath = "/cygdrive/";
-            TempPath += MyPath;
-            try {
-                WriteDirectoryFiles(TempPath);
-
-            } catch (std::exception& e) {
-                cout << e.what() << endl;
-                continue;
-            }
-            FullPath = TempPath;
-        } else if(command == "copy"){
-            string from;
-            string to;
+        } else if(command == "copy"){ //при копировании нужно указывать абсолютные пути файлов
+            fs::path from, to;
             cin >> from >> to;
-            from.insert(0, "/cygdrive/");
-            to.insert(0, "/cygdrive/"); //set_path d
             try {
-                fs::copy(from, to);
+                copy(from, to);
                 cout << "successfully" << endl;
-            } catch (std::exception& e) {
+            } catch (std::exception &e) {
                 cout << e.what() << endl;
             }
         } else if(command == "delete"){
             string name;
             cin >> name;
             fs::path temp = FullPath;
-            temp += '/' + name;
+            temp += '\\' + name;
             fs::remove_all(temp);
             cout << "successfully" << endl;
         } else if(command == "rename"){
             string from;
             string to;
             cin >> from >> to;
-            string temp = FullPath;
-            temp += '/';
-            fs::rename(temp+from, temp+to);
-            cout << "successfully" << endl;
-        } else if(command == "move_file"){
-            string from;
-            string to;
-            cin >> from >> to; //записываю абсолютные пути файла и папки в которую он будет перемещён
-            string temp = FullPath;
-            temp += '/' + from;
-            from = temp;
-            temp = FullPath;
-            temp += '/' + to;
-            to = temp;
+            try {
+                fs::rename(FullPath/from, FullPath/to);
+                cout << "successfully" << endl;
+            } catch (std::exception &e) {
+                cout << e.what() << endl;
+            }
+        } else if(command == "move"){
+            fs::path from, to;
+            cin >> from >> to; //считываю абсолютные пути файла и папки в которую он будет перемещён
+            to /= from.filename();
             try {
                 fs::copy(from, to);
             } catch (std::exception& e) {
@@ -156,21 +156,6 @@ int main() {
                 continue;
             }
             fs::remove_all(from);
-            cout << "successfully" << endl;
-        } else if(command == "move_directory"){
-            string from;
-            fs::path to;
-            cout << "Введите абсолютные пути папок" << endl;
-            cin >> from >> to; //записываю абсолютные пути файла и папки в которую он будет перемещён
-            to = "/cygdrive" / to;
-            from.insert(0, "/cygdrive/");
-            CreateDirectory(from.substr(from.rfind('/')+1), to);
-            try {
-                fs::rename(from, to / from.substr(from.rfind('/')+1));
-            } catch (std::exception& e) {
-                cout << e.what() << endl;
-                continue;
-            }
             cout << "successfully" << endl;
         } else {
             cout << "Unknown command" << endl;
